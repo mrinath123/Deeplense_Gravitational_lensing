16b7ec6b7d719b69281a4eac701514d16c0f3267
Mriganka Nath <mrinath123@gmail.com>
2022-09-08 21:42:25 +0530

    equivariant CNN

--- Lensing_DomainAdaptation/models.py
+++ Lensing_DomainAdaptation/models.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import timm
+from e2cnn import gspaces
+import e2cnn.nn as e2nn
 
 pretrained_model1 = 'tf_efficientnet_b2_ns'
 pretrained_model2 = 'resnet34d'
@@ -42,6 +44,104 @@ class Encoder(nn.Module):
         image = self.do(image)
         image = self.prelu(self.lin(image))
         return image
+
+class ECNN(nn.Module):
+    def __init__(self, features_size= latent_size, sym_group='Dihedral', N=2):
+        """
+        taken from https://arxiv.org/abs/2112.12121
+        """
+
+        super(Encoder, self).__init__()
+
+        if sym_group == 'Dihedral':
+            self.r2_act = gspaces.FlipRot2dOnR2(N=N)
+
+        elif sym_group == 'Circular':
+            self.r2_act = gspaces.Rot2dOnR2(N=N)
+
+        in_type = e2nn.FieldType(self.r2_act, [self.r2_act.trivial_repr])
+        self.input_type = in_type
+
+        out_type = e2nn.FieldType(self.r2_act, 24*[self.r2_act.regular_repr])
+        self.block1 = e2nn.SequentialModule(
+            e2nn.MaskModule(in_type, 150, margin=1),
+            e2nn.R2Conv(in_type, out_type, kernel_size=7, padding=1, bias=False),
+            e2nn.InnerBatchNorm(out_type),
+            e2nn.ReLU(out_type, inplace=True)
+        )
+
+        in_type = self.block1.out_type
+        out_type = e2nn.FieldType(self.r2_act, 48*[self.r2_act.regular_repr])
+        self.block2 = e2nn.SequentialModule(
+            e2nn.R2Conv(in_type, out_type, kernel_size=5, padding=2, bias=False),
+            e2nn.InnerBatchNorm(out_type),
+            e2nn.ReLU(out_type, inplace=True)
+        )
+        self.pool1 = e2nn.SequentialModule(
+            e2nn.PointwiseAvgPoolAntialiased(out_type, sigma=0.66, stride=2)
+        )
+
+        in_type = self.block2.out_type
+        out_type = e2nn.FieldType(self.r2_act, 48*[self.r2_act.regular_repr])
+        self.block3 = e2nn.SequentialModule(
+            e2nn.R2Conv(in_type, out_type, kernel_size=5, padding=2, bias=False),
+            e2nn.InnerBatchNorm(out_type),
+            e2nn.ReLU(out_type, inplace=True)
+        )
+
+        in_type = self.block3.out_type
+        out_type = e2nn.FieldType(self.r2_act, 96*[self.r2_act.regular_repr])
+        self.block4 = e2nn.SequentialModule(
+            e2nn.R2Conv(in_type, out_type, kernel_size=5, padding=2, bias=False),
+            e2nn.InnerBatchNorm(out_type),
+            e2nn.ReLU(out_type, inplace=True)
+        )
+        self.pool2 = e2nn.SequentialModule(
+            e2nn.PointwiseAvgPoolAntialiased(out_type, sigma=0.66, stride=2)
+        )
+
+        in_type = self.block4.out_type
+        out_type = e2nn.FieldType(self.r2_act, 96*[self.r2_act.regular_repr])
+        self.block5 = e2nn.SequentialModule(
+            e2nn.R2Conv(in_type, out_type, kernel_size=5, padding=2, bias=False),
+            e2nn.InnerBatchNorm(out_type),
+            e2nn.ReLU(out_type, inplace=True)
+        )
+
+        in_type = self.block5.out_type
+        out_type = e2nn.FieldType(self.r2_act, 64*[self.r2_act.regular_repr])
+        self.block6 = e2nn.SequentialModule(
+            e2nn.R2Conv(in_type, out_type, kernel_size=5, padding=1, bias=False),
+            e2nn.InnerBatchNorm(out_type),
+            e2nn.ReLU(out_type, inplace=True)
+        )
+        self.pool3 = e2nn.PointwiseAvgPoolAntialiased(out_type, sigma=0.66, stride=1, padding=0)
+
+        self.gpool = e2nn.GroupPooling(out_type)
+        c = self.gpool.out_type.size
+
+        self.fully_net = nn.Sequential(
+            nn.Linear(61504, features_size),
+            nn.BatchNorm1d(features_size),
+            nn.ELU(inplace=True),
+            nn.Linear(features_size, features_size),
+        )
+
+    def forward(self, x):
+        x = e2nn.GeometricTensor(x, self.input_type)
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.pool1(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.pool2(x)
+        x = self.block5(x)
+        x = self.block6(x)
+        x = self.pool3(x)
+        x = self.gpool(x)
+        x = x.tensor
+        x = self.fully_net(x.reshape(x.shape[0], -1))
+        return 
 
 class Classifier(nn.Module):
     def __init__(self,latent_size =  latent_size):
